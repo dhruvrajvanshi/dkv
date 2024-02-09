4ec2fa3d77540b4f227292ba276d3912f0be65ec
Dhruv Rajvanshi <d@rajv.dev>
2024-02-09 08:20:10 +0100

    Remove unnecessary module

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,46 +1,41 @@
+use dkv_protocol::{self, Value};
 use std::net::TcpStream;
 
-use dkv_protocol::Value;
+type Result<T> = dkv_protocol::Result<T>;
 
-fn main() -> dkv::Result<()> {
+fn main() -> Result<()> {
     let stream = TcpStream::connect("localhost:6543")?;
-    let mut djs = dkv::Client::new(stream);
+    let mut djs = Client::new(stream);
     djs.put(Value::from("key"), Value::from("value"))?;
     let value = djs.get(Value::from("key"))?;
     assert_eq!(value, Value::from("value"));
     Ok(())
 }
-mod dkv {
-    use dkv_protocol::{self, Value};
-    use std::net::TcpStream;
-
-    pub type Result<T> = dkv_protocol::Result<T>;
-    pub struct Client {
-        stream: TcpStream,
+pub struct Client {
+    stream: TcpStream,
+}
+impl Client {
+    pub fn new(stream: TcpStream) -> Client {
+        Client { stream }
     }
-    impl Client {
-        pub fn new(stream: TcpStream) -> Client {
-            Client { stream }
-        }
 
-        pub fn put(&mut self, key: Value, value: Value) -> Result<()> {
-            self.write(Value::from("PUT"))?;
-            self.write(key)?;
-            self.write(value)?;
-            return Ok(());
-        }
-        pub fn get(&mut self, key: Value) -> Result<Value> {
-            self.write(Value::from("GET"))?;
-            self.write(key)?;
-            return self.read_get_result();
-        }
+    pub fn put(&mut self, key: Value, value: Value) -> Result<()> {
+        self.write(Value::from("PUT"))?;
+        self.write(key)?;
+        self.write(value)?;
+        return Ok(());
+    }
+    pub fn get(&mut self, key: Value) -> Result<Value> {
+        self.write(Value::from("GET"))?;
+        self.write(key)?;
+        return self.read_get_result();
+    }
 
-        fn write(&mut self, value: Value) -> Result<()> {
-            value.write(&mut self.stream)
-        }
+    fn write(&mut self, value: Value) -> Result<()> {
+        value.write(&mut self.stream)
+    }
 
-        fn read_get_result(&mut self) -> Result<Value> {
-            Value::read(&mut self.stream)
-        }
+    fn read_get_result(&mut self) -> Result<Value> {
+        Value::read(&mut self.stream)
     }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,33 @@
-use dkv_protocol::Result;
-use std::net::TcpListener;
+use dkv_protocol::Command;
+use std::net::{TcpListener, TcpStream};
 
 fn main() -> Result<()> {
-    let mut server = dkv::Server::new(TcpListener::bind("0.0.0.0:6543")?);
+    let mut server = Server::new(TcpListener::bind("0.0.0.0:6543")?);
     println!("Listening on port 6543");
     server.start()?;
     Ok(())
 }
 
-pub mod dkv {
-    use std::net::{TcpListener, TcpStream};
-
-    use dkv_protocol::Command;
-    pub struct Server {
-        listener: TcpListener,
+pub struct Server {
+    listener: TcpListener,
+}
+type Result<T> = dkv_protocol::Result<T>;
+impl Server {
+    pub fn new(listener: TcpListener) -> Server {
+        Server { listener }
     }
-    type Result<T> = dkv_protocol::Result<T>;
-    impl Server {
-        pub fn new(listener: TcpListener) -> Server {
-            Server { listener }
-        }
 
-        pub fn start(&mut self) -> Result<()> {
-            for stream in self.listener.incoming() {
-                println!("Accepted new connection");
-                let command = self.read_command(&mut stream?)?;
-                dbg!(command);
-                todo!()
-            }
-            Ok(())
+    pub fn start(&mut self) -> Result<()> {
+        for stream in self.listener.incoming() {
+            println!("Accepted new connection");
+            let command = self.read_command(&mut stream?)?;
+            dbg!(command);
+            todo!()
         }
+        Ok(())
+    }
 
-        fn read_command(&self, stream: &mut TcpStream) -> Result<Command> {
-            Command::read(stream)
-        }
+    fn read_command(&self, stream: &mut TcpStream) -> Result<Command> {
+        Command::read(stream)
     }
 }

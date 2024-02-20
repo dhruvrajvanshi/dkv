9232afcdaa63217524b4e4cf708a34f6c9351380
Dhruv Rajvanshi <d@rajv.dev>
2024-02-20 07:50:14 +0100

    Add html version of test summary

--- parse_test_log.py
+++ parse_test_log.py
@@ -1,7 +1,8 @@
 import sys
 import re
+import json
 def main():
-    lines = [line for line in sys.stdin.readlines() if line.startswith("Summary")]
+    lines = sys.stdin.readlines()
     # example line:
     # Summary: version: 1.0.0, total tests: 50, passed: 5, rate: 10.00%
     # extract the version, total tests, passed, and rate
@@ -10,17 +11,76 @@ def main():
     total = 0
     passed = 0
     for line in lines:
+        if not line.startswith("Summary"):
+            continue
         total += int(regex.match(line).group("total"))
         passed += int(regex.match(line).group("passed"))
 
     print(f"Total tests: {total}, Passed: {passed}, Rate: {passed/total*100:.2f}%")
     with open("pages/test_summary.json", "w") as f:
-        import json
         json.dump({
             "schemaVersion": 1,
             "label": "Redis compatibility",
-            "message": f"{passed}/{total} tests passed",
+            "message": f"{passed/total*100:.2f}% tests passed",
         }, f)
+
+    test_suite = []
+    with open("compatibility-test-suite-for-redis/cts.json") as f:
+        test_suite = json.load(f)
+    test_suite_by_name = {test["name"]: test for test in test_suite}
+
+    with open("pages/test_summary.html", "w") as f:
+        regex = re.compile(r"test: (?P<test>.+) (?P<result>(passed|failed|skipped))")
+        rows = ""
+        for line in lines:
+            if not line.startswith("test: "):
+                continue
+            match = regex.match(line)
+            test = match.group("test").removesuffix(" tags")
+            result = match.group("result")
+            test_json = json.dumps(test_suite_by_name[test], indent=2)
+            emoji = {
+                "passed": "✅",
+                "failed": "❌",
+                "skipped": "⚠️",
+            }[result]
+            rows += f"""
+                <tr>
+                    <td>
+                        <details style="margin-bottom: 0">
+                            <summary>{test}</summary>
+                            <pre><code class="language-json">{test_json}</code></pre>
+                        </details>
+                    </td>
+                    <td>{emoji}&nbsp;{result.capitalize()}</td>
+                </tr>
+            """
+        f.write(f"""
+        <html>
+        <head>
+            <title>Redis compatibility test results</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
+        </head>
+        <body>
+            <main class="container">
+                <h1>Redis compatibility test results</h1>
+                <h2>Summary</h2>
+                <p>{passed}/{total} ({passed/total:.2f}%) tests passed</p>
+                <progress value="{passed}" max="{total}">{passed}/{total}</progress>
+                <table>
+                    <tr>
+                        <th>Test</th>
+                        <th>Result</th>
+                    </tr>
+                    {rows}
+                </table>
+            </main>
+
+            <link href="prism.css" rel="stylesheet" />
+            <script src="prism.js"></script>
+        </body>
+        </html>
+        """)
 
 if __name__ == '__main__':
     main()

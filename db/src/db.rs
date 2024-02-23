8014d285caedc56f9eec69646cb217479617be48
Dhruv Rajvanshi <d@rajv.dev>
2024-02-23 10:59:13 +0100

    Add pub sub to database

--- db/src/db.rs
+++ db/src/db.rs
@@ -40,6 +40,7 @@ impl DB {
         DB {
             db_impl: Arc::new(Mutex::new(DBImpl {
                 map: HashMap::new(),
+                subscribers: HashMap::new(),
             })),
         }
     }
@@ -83,14 +84,41 @@ impl DB {
         let value = m.get_mut(key);
         f(value)
     }
+
+    fn with_impl<T>(&self, f: impl FnOnce(&mut DBImpl) -> T) -> T {
+        let mut db_impl = self.db_impl.lock().unwrap();
+        f(&mut db_impl)
+    }
+
+    pub fn publish(&self, channel: &str, value: &str) {
+        let subscribers =
+            self.with_impl(|db| db.subscribers.get(channel).unwrap_or(&vec![]).clone());
+        // Subscriber functions may run for a long time, so we don't want to hold the lock
+        // while they run
+        for subscriber in subscribers {
+            subscriber(channel, value)
+        }
+    }
+
+    pub fn subscribe(&self, channel: &str, f: impl Fn(&str, &str) + Send + Sync + 'static) {
+        self.with_impl(move |db| {
+            if !db.subscribers.contains_key(channel) {
+                db.subscribers.insert(channel.to_string(), vec![]);
+            }
+            db.subscribers.get_mut(channel).unwrap().push(Arc::new(f));
+        })
+    }
 }
 
 struct DBImpl {
     map: HashMap<String, Value>,
+    subscribers: HashMap<String, Vec<Arc<dyn Fn(&str, &str) + Send + Sync + 'static>>>,
 }
 
 #[cfg(test)]
 mod test {
+    use std::thread::spawn;
+
     use super::*;
 
     #[test]
@@ -102,5 +130,32 @@ mod test {
             _ => panic!(),
         });
         assert_eq!(len, "value".len());
+    }
+
+    #[test]
+    fn should_allow_pub_sub() {
+        let db = DB::new();
+        let db_publisher = db.clone();
+        let count = Arc::new(Mutex::new(0));
+        {
+            let count = count.clone();
+            db.subscribe("channel", move |_, message| {
+                *count.clone().lock().unwrap() += 1;
+                let count = *count.clone().lock().unwrap();
+                assert_eq!(message, format!("message{}", count));
+            });
+        }
+        let publisher = spawn(move || {
+            let db = db_publisher;
+            db.publish("channel", "message1");
+            db.publish("channel", "message2");
+            db.publish("channel", "message3");
+            println!("Published messages");
+
+            drop(db);
+        });
+
+        publisher.join().unwrap();
+        assert_eq!(3, *count.lock().unwrap());
     }
 }

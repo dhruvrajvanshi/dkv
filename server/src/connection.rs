1d3f4e9bda2bc38160a183b7244cae2af430b5e4
Dhruv Rajvanshi <d@rajv.dev>
2024-03-02 13:28:41 +0100

    [refactor]: Create a Connection struct
    A connection might want to have state. Making a struct out
    of it is helpful.

--- server/src/connection.rs
+++ server/src/connection.rs
@@ -3,21 +3,51 @@ use tracing::{debug, error};
 
 use crate::protocol;
 
-pub async fn handle_connection(socket: TcpStream, _connection_id: usize) -> tokio::io::Result<()> {
-    let (reader, mut writer) = tokio::io::split(socket);
-    let mut reader = tokio::io::BufReader::new(reader);
+pub async fn handle_connection(socket: TcpStream, connection_id: usize) -> tokio::io::Result<()> {
+    let mut conn = Connection::new(socket, connection_id);
     loop {
         debug!("handle_connection: Loop");
-        match protocol::parse_array(&mut reader).await {
-            Ok(arr) => {
-                debug!("Received command: {:?}", arr);
-                protocol::write_error_string(&mut writer, "Unimplemented").await?;
-            }
-            Err(e) => {
-                error!("Error parsing message: {:?}", e);
+        match conn.tick().await {
+            Ok(TickResult::Continue) => {}
+            Ok(TickResult::Close) => break,
+            Err(_) => {
                 break;
             }
         }
     }
     Ok(())
+}
+
+struct Connection {
+    _id: usize,
+    reader: tokio::io::BufReader<tokio::io::ReadHalf<TcpStream>>,
+    writer: tokio::io::WriteHalf<TcpStream>,
+}
+enum TickResult {
+    Continue,
+    Close,
+}
+impl Connection {
+    fn new(socket: TcpStream, id: usize) -> Self {
+        let (reader, writer) = tokio::io::split(socket);
+        let reader = tokio::io::BufReader::new(reader);
+        Connection {
+            _id: id,
+            reader,
+            writer,
+        }
+    }
+    async fn tick(&mut self) -> tokio::io::Result<TickResult> {
+        Ok(match protocol::parse_array(&mut self.reader).await {
+            Ok(arr) => {
+                debug!("Received command: {:?}", arr);
+                protocol::write_error_string(&mut self.writer, "Unimplemented").await?;
+                TickResult::Continue
+            }
+            Err(e) => {
+                error!("Error parsing message: {:?}", e);
+                TickResult::Close
+            }
+        })
+    }
 }
